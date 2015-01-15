55474bacd8e169c780920ee1e060f50c498d697e
Anjanavr <anjana@qburst.com>
2015-01-15 18:07:38 +0530

    checkbox and date formatter

--- js/emp.js
+++ js/emp.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
+	if(21>125){
+		console.log("greater");
+	}
+	else{
+		console.log("lesser");
+	}
+
 	$.getJSON("../json/emp.json", function(getdetails) {
 
 	//adding headers
@@ -7,41 +14,58 @@ $(document).ready(function () {
 		var fields = getdetails.tabheader;
 		var row = $("<tr />");
 		$("#tab").append(row);
+		 //row.append($("<th>" + " " +"</th>" ));
 		for(i=0; i<fields.length; i++) {
+			if(i == 0) {
+				row.append($("<th>" + fields[i].header + "</th>"));;
+			}else {
 			row.append($("<th>" + fields[i].header + "<i class='icon-down-dir'></i><i class='icon-up-dir'></i></th>"));
 		    $(".icon-up-dir").hide();
+			}
         }
+       
 		
-		for(i=0;i<fields.length;i++) {
+		for(i=1;i<fields.length;i++) {
 
 			$("#label_div").append($("<label>" + fields[i].header + ":</label>" +"<br />"));
-			$("#input_div").append($("<input id = " +fields[i].name + " " + "type=" + fields[i].type +  " " + "name=" + fields[i].name + ">" ));
+
+			if(fields[i].type == "date(dd/mm/yyyy)"){
+				$("#input_div").append($("<input id = " +fields[i].name + " " + "type=" + fields[i].type +  " " + "placeholder=" + "DD/MM/YYYY" + " " + "name=" + fields[i].name +" " + "required>" ));
+			}
+			else{
+				$("#input_div").append($("<input id = " +fields[i].name + " " + "type=" + fields[i].type +  " " + "name=" + fields[i].name + " " + "required>" ));
+			}
 		}
          
     //adding employee details
 		$.each(getdetails.employees , function(key,value) {
 			var row = $("<tr />");
 			$("#tab").append(row);
+			 //row.append($("<td>" + "<input type=checkbox>" +"</td>" ));
 			$.each(value ,function(k,v) {
 				row.append ($("<td>" + v + "</td>" ));
 			});
+			 // row.append($("<td>" + "<input type=checkbox>" +"</td>" ));
 		});
 		
 	//adding label and input fields to the form
 
-		for(i=0;i<fields.length;i++) {
+		for(i=1;i<fields.length;i++) {
 
 			$("#labelDiv").append($("<label>" + fields[i].header + ":</label>" +"<br />"));
-
-			$("#inputDiv").append($("<input id = " +fields[i].name + " " + "type=" + fields[i].type +  " " + "name=" + fields[i].name + " " + "required>" ));
+			if(fields[i].type == "date(dd/mm/yyyy)"){
+				$("#inputDiv").append($("<input id = " +fields[i].name + " " + "type=" + fields[i].type +  " " + "placeholder=" + "DD/MM/YYYY" + " " + "name=" + fields[i].name + " " + "required>" ));
+			}
+			else {
+				$("#inputDiv").append($("<input id = " +fields[i].name +  " " + "type=" + fields[i].type +  " " + "name=" + fields[i].name + " " + "required>" ));
+			}	
 		}
-
 	// adding data to localStorage	and appending it to the table
 		
 		var s = 0;
 		if( typeof(Storage) != "undefined") {
 
-			$("#addEmpBtn").click(function() {
+			$("#addEmpForm").on( "submit" , function() {
 				var values = {};
 				$("#formfields input").each(function() {
 					var getInputName = $(this).attr('name');
@@ -62,54 +86,162 @@ $(document).ready(function () {
 		        localStorage.setItem("index" , s);
 
 			});
+		    
 		    for( var i=0; i < (parseInt(localStorage.getItem("index"))); i++ ) {
-		        	var row = $("<tr />");
-					$("#tab").append(row);
-					formData = localStorage.getItem(i);
-					$.each(JSON.parse(formData) , function (k,v) {
-						row.append ($("<td>" + v + "</td>" ));
-					})
+		        var row = $("<tr />");
+				$("#tab").append(row);
+				row.append($("<td>" + "<input type=checkbox>" +"</td>" ));
+				formData = localStorage.getItem(i);
+				$.each(JSON.parse(formData) , function (k,v) {
+					row.append ($("<td>" + v + "</td>" ));
+
+				});
+				
 		    }   	
 		}
 		else {
 		    document.getElementById("tablemain").innerHTML = "Sorry, your browser does not support web storage...";
 		}
+		$("#Age").focusout(function() {
+
+			var getInputValue = $(this).val();
+			if(getInputValue < 1) {
+				alert("invalid Age");
+				$(this).val("");
+			}
+		});
+
+		$("#DOB").focusout(function() {
+			var d=new Date();
+			var year = d.getFullYear();		   			   
+			var getvalue = $(this).val();
+			var dataLength = getvalue.length;
+			dataLength = dataLength-4;
+			var datepattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+			var checkpattern = getvalue.match(datepattern); 
+			if(checkpattern == null){
+
+
+				dd= getvalue.substr(0,2);
+				mm = getvalue.substr(2,2);
+				y  = getvalue.substr(4,dataLength)
+				if (mm < 1 || mm > 12) {
+					alert("invalid date");
+					$(this).val("");
+				}
+
+				else if (dd < 1 || dd> 31) {
+					alert("invalid date");$(this).val("");
+				}
+
+				else  if(y>year || y.length != 4) {
+					alert("invalid date") ;
+					$(this).val("");
+				}
+				else if ((mm==4 || mm==6 || mm==9 || mm==11) && dd ==31) 
+				{
+					alert("invalid date");$(this).val("");
+				}
+
+				else if (mm == 2) 
+				{  
+					var isleap = (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));
+					if (dd> 29 || (dd ==29 && !isleap)) 
+						alert("invalid");$(this).val("");
+				}      
+
+				else{
+					$(this).val(dd+"/"+mm+"/"+y);
+
+				}
+			}
+			else{
+				dd= getvalue.split("/")[0];
+				mm = getvalue.split("/")[1];
+				y  = getvalue.split("/")[2];
+				if(dd.length < 2)
+				{
+				dd="0"+dd;
+				}
+				if(mm.length < 2){
+				mm="0"+mm;
+				}
+
+				if (mm < 1 || mm > 12) {
+				alert("invalid date");
+				$(this).val("");
+				}
+
+				else if (dd < 1 || dd> 31) {
+				alert("invalid date");$(this).val("");
+				}
+
+				else  if(y>year || y.length < 4)
+				{
+				alert("invalid date") ;$(this).val("");
+				}
+				else if ((mm==4 || mm==6 || mm==9 || mm==11) && dd ==31) 
+				{  
+				alert("invalid date");$(this).val("");
+				}
+
+				else if (mm == 2) 
+				{  
+					var isleap = (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));
+					if (dd> 29 || (dd ==29 && !isleap)) 
+
+					{
+						alert("invalid date");
+					    $(this).val("");
+				    }
+				}      
+
+				if($(this).val()!="") {
+					$(this).val(dd+"/"+mm+"/"+y);
+				}
+			}
+		});
 						
 	// Adding ROWS
-		 $('#adddetails').on("click", function(){
-	         var row = $("<tr />");
-			 $("#tab").append(row);
-	       
-	         $("#field input").each(function() {
-		         row.append ($("<td>" + $(this).val() + "</td>" ));
-			});
-	   	});
+	// var values = {};
+	// $("#addpeople").on( "submit" , function(){
+	//        var row = $("<tr />");
+	// 	$("#tab").append(row);	       
+	//        $("#field input").each(function() {
+	//         row.append ($("<td>" + $(this).val() + "</td>" ));
+	// 	});
+	//   });
 
 	//clear row
+	  // 	$('#clearrow').on("click", function(){
 
-	  	$('#clearrow').on("click", function(){
-		
-			if($('#tab tr').size()>1){
-				$('#tab tr:last-child').remove();
-			}
-			else{
-				alert('One row should be present in table');
-			}
-		});
-
-	  	$('#deleteRow').on("click", function(){
-		
-			if($('#tab tr').size()>1){
-				$('#tab tr:last-child').remove();
-				var d = parseInt(localStorage.getItem("index"));
-				d = d-1; 
-				localStorage.removeItem(d);
-				localStorage.setItem("index" , d);
-			}
-			else{
-				alert('One row should be present in table');
-			}
-		});
+			// var rowCount=$("#tab tr").length;
+			// for(var i=0;i<rowCount;i++){
+			// 	var row=$("tab tr").rows[i];
+			// 	var chkbox=row.cells[5].childNodes[0];
+			// 	if( null != chkbox && true == chkbox.checked ){
+			// 		if(rowCount<=1){
+			// 			alert("Cannot delete all the rows.");
+			// 			break;
+			// 		}
+	  //               table.deleteRow(i);
+	  //               rowCount--;
+	  //               i--;
+	  //           }
+	  //       }
+			// if($('#tab tr').size()>1){
+			// 	$('#tab tr:last-child').remove();
+			// 	var d = parseInt(localStorage.getItem("index"));
+			// 	if(d) {
+			// 		d = d-1; 
+			// 		localStorage.removeItem(d);
+			// 		localStorage.setItem("index" , d);
+			// 	}
+			// }
+			// else{
+			// 	alert('One row should be present in table');
+			// }
+		//});
 
 	//sorting
 
@@ -123,8 +255,10 @@ $(document).ready(function () {
 			var rows = table.find('tr').toArray();
 			rows.shift(); //remove first element which has th
 			var index=$(this).index();
+			if(index != 0) {
+				//index = index -1;
 			$.each(getdetails.tabheader , function(key,header) {
-				if(index == key){
+				if(index == (key)){
 
 					var headertype = header.type;
 
@@ -140,7 +274,6 @@ $(document).ready(function () {
 			 		       		 temp = rows[j];
 			 		       		rows[j] = rows[j+1];
 			 		       		rows[j+1] = temp;
-			 		       		console.log(rows[j],"sorting")
 			                 }	 		     
 			 		    }
 			 		}
@@ -153,17 +286,50 @@ $(document).ready(function () {
 					}
 				}
 		
-				if((headertype=="number") || (headertype=="string")) {					
-					var temp = new Array();					
+				if(headertype == "number") {					
+					var temp;				
 					for (var k=0;k<(rows.length-1);k++) {  			 		      
-			 		    for (var j=0;j<(rows.length-k-1);j++) {			 		       	
-			 		       	if(rows[j].cells[index].innerHTML.toLowerCase() > rows[j+1].cells[index].innerHTML.toLowerCase()) {
+			 		    for (var j=0;j<(rows.length-k-1);j++) {	
+
+			 		    	console.log (rows[j].cells[index].innerHTML , rows[j+1].cells[index].innerHTML,"compare");
+			 		    	//if (rows[j+1].cells[index].innerHTML) {			 		    			 		       	
+			 		       	if(parseInt(rows[j].cells[index].innerHTML) > parseInt(rows[j+1].cells[index].innerHTML)) {
+			 		       		//console.log (rows[j].cells[index].innerHTML.toLowerCase() , rows[j+1].cells[index].innerHTML.toLowerCase());
+			 		       		 console.log("true");
 			 		       		 temp = rows[j];
 			 		       		rows[j] = rows[j+1];
 			 		       		rows[j+1] = temp;
 			                }
+			            	//}
 			 		    }
+
 			 		}
+			 	
+					this.asc = !this.asc;
+					if (!this.asc){
+						rows = rows.reverse();
+					}
+					for (i = 0; i < rows.length; i++){
+						table.append(rows[i]);	
+					}
+				}
+				if(headertype == "string") {					
+					var temp = new Array();				
+					for (var k=0;k<(rows.length-1);k++) {  			 		      
+			 		    for (var j=0;j<(rows.length-k-1);j++) {	
+			 		    
+			 		    	if (rows[j+1].cells[index].innerHTML) {			 		    			 		       	
+			 		       	if(rows[j].cells[index].innerHTML.toLowerCase() > rows[j+1].cells[index].innerHTML.toLowerCase()) {
+			 		       		console.log (rows[j].cells[index].innerHTML.toLowerCase() , rows[j+1].cells[index].innerHTML.toLowerCase());
+			 		       		 temp = rows[j];
+			 		       		rows[j] = rows[j+1];
+			 		       		rows[j+1] = temp;
+			                }
+			            	}
+			 		    }
+
+			 		}
+			 	
 					this.asc = !this.asc;
 					if (!this.asc){
 						rows = rows.reverse();
@@ -173,7 +339,24 @@ $(document).ready(function () {
 					}
 				}
 			});
+			}
     	});
 	});
 });
-       
+function deleteRow() {
+	var table=document.getElementById("tab");
+	var rowCount=table.rows.length;
+	for(var i=0;i<rowCount;i++){
+		var row=table.rows[i];
+		var chkbox=row.cells[0].childNodes[0];
+		if(null!=chkbox&&true==chkbox.checked){
+			if(rowCount<1){
+				alert("Cannot delete all the rows.");
+				break;
+			}
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+        }
+    }
+}	
